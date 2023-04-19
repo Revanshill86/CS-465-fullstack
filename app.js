6f3f8354083865ecab13d070807dd978642f0d35
Revanshill86 <Randolph.evanshill@snhu.edu>
2023-04-19 00:45:37 -0500

    Corrected page rendering issues

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
+require("./app_api/models/db"); //Trigger database connection and mongoose schema models to be loaded
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel'); 
 const roomRouter = require('./app_server/routes/rooms');
-const hbs = require('hbs');
 
-require("./app_api/models/db");
+
+
 
 const apiRouter = require('./app_api/routes/index');
 const app = express();
@@ -28,6 +30,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//allow CORS
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const model = mongoose.model('trips');
 
 //Get /trips lists all the trips
-const tripsList = async(req, res) => {
+const tripsList = async(req, res) => 
+{
     model
         .find({})
         .exec((err, trips) => 
@@ -27,12 +28,12 @@ const tripsList = async(req, res) => {
 
 };
 
-const tripsFindCode= async(req, res) => {
+const tripsFindCode = async(req, res) => {
     model
         .find({ 'code': req.params.tripCode })
-        .exec((err, trips) => 
+        .exec((err, trip) => 
         {
-            if (!trips)
+            if (!trip)
             {
                 return res
                     .status(404)
@@ -46,15 +47,54 @@ const tripsFindCode= async(req, res) => {
             else{
                 return res
                     .status(200)
-                    .json(trips);
+                    .json(trip);
             }
         });
 
 };
 
+const tripsUpdateTrip = async (req, res) => 
+{    console.log(req.body);    
+    model          
+        .findOneAndUpdate({ 'code': req.params.tripCode }, 
+        {            
+            code: req.body.code,            
+            name: req.body.name,            
+            length: req.body.length,            
+            start: req.body.start,            
+            resort: req.body.resort,            
+            perPerson: req.body.perPerson,            
+            image: req.body.image,            
+            description: req.body.description       
+         }, { new: true })        
+         .then(trip => {            
+            if (!trip) {                
+                return res                    
+                .status(404)                    
+                .send({                        
+                    message: "Trip not found with code " + req.params.tripCode                    
+                });            
+            }            
+            res.send(trip);        
+        }).catch(err => {            
+            if (err.kind === 'ObjectId') {                
+                return res                    
+                .status(404)                    
+                .send({                        
+                    message: "Trip not found with code " + req.params.tripCode                    
+                });            
+            }            
+            return res                
+            .status(500) // server error                
+            .json(err);        
+        }); 
+    }
+
 
 module.exports = 
 {
     tripsList,
-    tripsFindCode
+    tripsFindCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
